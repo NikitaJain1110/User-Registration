52163452c001718b5f737585da7899abcd6a9b92
NikitaJain1110 <56667079+NikitaJain1110@users.noreply.github.com>
2020-11-04 19:47:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+function App(){
+  return(
+    <div>
+      <SignUp/>
+    </div>
+  )
+}
+
+export default App;
